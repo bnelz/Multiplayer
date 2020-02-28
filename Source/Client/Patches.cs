e7f2ff852cc017ceacd8e7512b34d5314f66ae1e
Brandon Nelson <brandon.lnelson@gmail.com>
2020-02-28 13:57:39 -0800

    Updates SortArchivablesById with inner private type reference

--- Source/Client/Patches.cs
+++ Source/Client/Patches.cs
@@ -1392,9 +1392,20 @@ namespace Multiplayer.Client
         }
     }
 
-    [HarmonyPatch(typeof(Archive), "<Add>m__2")]
+    [HarmonyPatch()]
     static class SortArchivablesById
     {
+        static MethodBase TargetMethod()
+        {
+            // Get all non public types, including compiler types
+            List<Type> nestedPrivateTypes = new List<Type>(typeof(Archive).GetNestedTypes(BindingFlags.NonPublic));
+
+            // There are two of these in 1.1, <GetGizmos> and <>c. Pluck the one we want for sort inner
+            Type cType = nestedPrivateTypes.Find(t => t.Name.Equals("<>c"));
+
+            return AccessTools.Method(cType, "<Add>b__6_0");
+        }
+
         static void Postfix(IArchivable x, ref int __result)
         {
             if (x is ArchivedDialog dialog)
